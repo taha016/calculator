66a423afe43e164151d482c6637f92ca144c0153
taha016 <msaddiqueshareef@gmail.com>
2025-01-28 23:24:30 +0500

    added Keyboard support and a very little css left

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ digits.forEach(digit => {
 
 equal.addEventListener('click', () => {
   display.value = operate(operator1, firstNum, secondNum) 
+  num = display.value
+  firstNum = parseFloat(num)
 })
 
 operatorBtn.forEach(operators => {
@@ -55,6 +57,11 @@ clear.addEventListener('click', () => {
 }) 
 
 function appendToDisplay(input) {
+  
+  if(input === '.' && display.value.includes('.')) return;
+
+  if(input === '0' && display.value === '0') return;
+
   display.value += input
   num = display.value
   parseInt(num)
@@ -89,3 +96,49 @@ function clearDisplay() {
   secondNum = 0
   operator1 = ''
 }
+
+// Added Keyboard Support
+document.addEventListener('keydown', (e) => {
+  const key = e.key;
+  // Prevent default action for specific keys
+  if (['Enter', 'Backspace', 'Escape'].includes(key)) e.preventDefault();
+
+  // Map keyboard keys to calculator functions
+  const keyMap = {
+      '0': '0', '1': '1', '2': '2', '3': '3', '4': '4',
+      '5': '5', '6': '6', '7': '7', '8': '8', '9': '9',
+      '+': '+', '-': '-', '*': 'x', '/': '/', // Map * to x for multiplication
+      '.': '.', 
+      'Enter': '=',
+      'Backspace': '⌫',
+      'Escape': 'C'
+  };
+
+  if (keyMap[key]) {
+      if (key === 'Enter') {
+          // Trigger equals
+          display.value = operate(operator1, firstNum, secondNum);
+          num = display.value;
+          firstNum = parseFloat(num);
+      } else if (key === 'Backspace') {
+          // Trigger backspace
+          display.value = display.value.slice(0, -1);
+          num = display.value;
+          if(operator1 === '') {
+              firstNum = parseFloat(num) || 0;
+          } else {
+              secondNum = parseFloat(num) || 0;
+          }
+      } else if (key === 'Escape') {
+          // Trigger clear
+          clearDisplay();
+      } else if (['+', '-', 'x', '/'].includes(keyMap[key])) {
+          // Handle operators
+          operator1 = keyMap[key];
+          display.value = '';
+      } else {
+          // Handle numbers and decimal
+          appendToDisplay(keyMap[key]);
+      }
+  }
+});
